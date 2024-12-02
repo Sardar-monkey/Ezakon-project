215ab25ec03c4e227c09074aa7aa917c55244f7b
Sardar <sardarhomatv@gmail.com>
2024-12-02 10:10:30 +0500

    Chat added

--- templates/JS/script.js
+++ templates/JS/script.js
@@ -1,4 +1,45 @@
 function ShowMenu() {
     const menu = document.getElementById("menu");
-    menu.style.display = (menu.style.display === "none" || menu.style.display === "") ? "flex" : "none";
+    
+    menu.classList.toggle("open");
+};
+
+
+const ChatForm = document.getElementById("chat-form");
+const ChatWindow = document.getElementById("chat-window");
+const UserInput = document.getElementById("user-input");
+
+function Message(text, sender) {
+    const DivBox = document.createElement('div');
+    DivBox.textContent = text;
+    DivBox.classList.add('chat-message', sender);
+    ChatWindow.appendChild(DivBox);
+    ChatWindow.scrollTop = ChatWindow.scrollHeight;
 }
+
+ChatForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const UserMessage = UserInput.value.trim();
+    Message(UserMessage, 'user')
+
+    try {
+        const response = await fetch('django_server_url', {
+            method : 'POST',
+            headers : {'Content-type' : 'application/json'},
+            body : JSON.stringify({message : UserMessage})
+        })
+
+        const data = await response.json();
+
+        Message(data.reply, 'bot');
+
+    } catch(error) {
+        Message("Ошибка при подключений к серверу", 'bot');
+    }
+
+    UserInput.value = "";
+
+})
+
+' beknur = {"REPLY" : "Beknur","surname" : 8632}'
